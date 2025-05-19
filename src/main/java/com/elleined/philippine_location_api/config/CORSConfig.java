9f50c4f584c0af2a8427ac65a09f13ca8fe850a5
Elleined <demadegu@gmail.com>
2025-05-19 20:45:32 +0800

    v2.0.0

---
+++ src/main/java/com/elleined/philippine_location_api/config/CORSConfig.java
@@ -0,0 +1,21 @@
+package com.elleined.philippine_location_api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Value("#{'${allowed-origins}'.split(',')}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET")
+                .maxAge(3600); // 1 hour
+    }
+}
