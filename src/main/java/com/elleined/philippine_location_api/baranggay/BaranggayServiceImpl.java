7f77176c87bf597ac1af26bb51076538418ddb57
Elleined <demadegu@gmail.com>
2025-05-19 19:06:14 +0800

    Added @validated in baranggay

--- src/main/java/com/elleined/philippine_location_api/baranggay/BaranggayServiceImpl.java
+++ src/main/java/com/elleined/philippine_location_api/baranggay/BaranggayServiceImpl.java
@@ -9,11 +9,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @Slf4j
 @Service
+@Validated
 @Transactional
 @RequiredArgsConstructor
 public class BaranggayServiceImpl implements BaranggayService {
