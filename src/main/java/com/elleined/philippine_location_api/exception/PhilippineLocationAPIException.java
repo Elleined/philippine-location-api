c7281f730979d5c802fad7a9dd5bce645c4abb71
Elleined <demadegu@gmail.com>
2025-07-20 09:17:25 +0800

    Rename MySQL service to dev-mysql-server in docker-compose.yml, change PhilippineLocationAPIException from RuntimeException to checked Exception, and add handler for RuntimeException in ExceptionController with BAD_REQUEST response

--- src/main/java/com/elleined/philippine_location_api/exception/PhilippineLocationAPIException.java
+++ src/main/java/com/elleined/philippine_location_api/exception/PhilippineLocationAPIException.java
@@ -1,6 +1,6 @@
 package com.elleined.philippine_location_api.exception;
 
-public class PhilippineLocationAPIException extends RuntimeException {
+public class PhilippineLocationAPIException extends Exception {
     public PhilippineLocationAPIException(String message) {
         super(message);
     }
