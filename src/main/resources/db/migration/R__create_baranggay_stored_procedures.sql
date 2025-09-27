8b54437ed54b654d2837a96467c152da81065ec0
elleined <demadegu@gmail.com>
2025-09-27 16:00:27 +0800

    Fix again the barangay stored procs

--- src/main/resources/db/migration/R__create_baranggay_stored_procedures.sql
+++ src/main/resources/db/migration/R__create_baranggay_stored_procedures.sql
@@ -1,13 +1,12 @@
 DROP PROCEDURE IF EXISTS baranggay_find_all_by;
 DELIMITER //
 CREATE PROCEDURE IF NOT EXISTS baranggay_find_all_by(
-    IN region_id BIGINT,
-    IN province_id BIGINT,
-    IN city_id BIGINT,
-    IN name VARCHAR(50),
+    IN region_id INT,
+    IN province_id INT,
+    IN city_id INT,
+    IN name VARCHAR(255),
     IN page INT,
-    IN size INT,
-    OUT out_total_elements INT
+    IN size INT
 )
 BEGIN
 	DECLARE offset INT;
@@ -33,19 +32,7 @@ BEGIN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'size cannot be a negative number or null';
     END IF;
 
-    SELECT COUNT(*)
-    INTO out_total_elements
-    FROM baranggay b
-    JOIN city c ON c.id = b.city_id
-    JOIN province p ON p.id = c.province_id
-    JOIN region r ON r.id = p.region_id
-    WHERE r.id = region_id
-    AND p.id = province_id
-    AND c.id = city_id
-    AND b.name LIKE CONCAT('%', IFNULL(NAME, ""), '%');
-
-    -- result set
-    SELECT b.id AS id, b.name AS name, b.city_id AS city_id
+    SELECT b.*
     FROM baranggay b
     JOIN city c ON c.id = b.city_id
     JOIN province p ON p.id = c.province_id
@@ -57,5 +44,38 @@ BEGIN
     ORDER BY b.name ASC
     LIMIT size
     OFFSET offset;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS baranggay_find_all_by_total;
+DELIMITER //
+CREATE PROCEDURE IF NOT EXISTS baranggay_find_all_by_total(
+    IN region_id INT,
+    IN province_id INT,
+    IN city_id INT,
+    IN name VARCHAR(255)
+)
+BEGIN
+    IF region_id IS NULL OR region_id <= 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'region id cannot be a negative number or null';
+    END IF;
+
+    IF province_id IS NULL OR province_id <= 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'province id cannot be a negative number or null';
+    END IF;
+
+    IF city_id IS NULL OR city_id <= 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'city id cannot be a negative number or null';
+    END IF;
+
+    SELECT COUNT(*)
+    FROM baranggay b
+    JOIN city c ON c.id = b.city_id
+    JOIN province p ON p.id = c.province_id
+    JOIN region r ON r.id = p.region_id
+    WHERE r.id = region_id
+    AND p.id = province_id
+    AND c.id = city_id
+    AND b.name LIKE CONCAT('%', IFNULL(name, ""), '%');
 END //
 DELIMITER ;
