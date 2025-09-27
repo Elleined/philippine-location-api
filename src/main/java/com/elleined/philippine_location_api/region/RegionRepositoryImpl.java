71b5592782b39e866fdf277e2f5f33450d5258a7
elleined <demadegu@gmail.com>
2025-09-27 16:43:20 +0800

    Added rowmappers and explicitly return the stored proc columns

--- src/main/java/com/elleined/philippine_location_api/region/RegionRepositoryImpl.java
+++ src/main/java/com/elleined/philippine_location_api/region/RegionRepositoryImpl.java
@@ -3,10 +3,14 @@ package com.elleined.philippine_location_api.region;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Repository
 @Transactional
@@ -21,7 +25,23 @@ public class RegionRepositoryImpl implements RegionRepository {
                                   int page,
                                   int size) {
 
-        return List.of();
+        String key = "key";
+
+        MapSqlParameterSource parameters = new MapSqlParameterSource()
+                .addValue("name", name)
+                .addValue("page", page)
+                .addValue("size", size);
+
+        Map<String, Object> result = new SimpleJdbcCall(jdbcTemplate)
+                .withProcedureName("barangay_find_all_by")
+                .withReturnValue()
+                .returningResultSet(key, rowMapper)
+                .execute(parameters);
+
+        @SuppressWarnings("unchecked")
+        List<Region> regions = (List<Region>) Objects.requireNonNullElse(result.get(key), List.of());
+
+        return regions;
     }
 
     @Override
