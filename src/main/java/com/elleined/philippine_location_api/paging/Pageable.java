cddebd401053eb5911a692fec4e60698c7eb9306
Elleined <demadegu@gmail.com>
2025-12-19 05:40:55 +0800

    - Added some additional fields for pagination

--- src/main/java/com/elleined/philippine_location_api/paging/Pageable.java
+++ src/main/java/com/elleined/philippine_location_api/paging/Pageable.java
@@ -1,6 +1,8 @@
-package com.elleined.philippine_location_api.paging;
+package com.pams.backend.paging;
 
+import com.elleined.philippine_location_api.paging.PageRequest;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.jspecify.annotations.Nullable;
 
 import java.util.Collection;
 
@@ -9,9 +11,30 @@ public record Pageable<T>(
         @JsonProperty("page_request") PageRequest request,
         @JsonProperty("total_elements") int totalElements
 ) {
+
     @JsonProperty("total_pages")
     public int getTotalPages() {
         return (totalElements + request.size() - 1) / request.size();
+    }
+
+    @JsonProperty("has_next_page")
+    public boolean hasNextPage() {
+        return request.page() < getTotalPages();
+    }
+
+    @JsonProperty("has_previous_page")
+    public boolean hasPreviousPage() {
+        return request.page() > 1 && getTotalPages() > 0;
+    }
+
+    @JsonProperty("next_page")
+    public @Nullable Integer getNextPage() {
+        return hasNextPage() ? request.page() + 1 : null;
+    }
+
+    @JsonProperty("previous_page")
+    public @Nullable Integer getPreviousPage() {
+        return hasPreviousPage() ? request.page() - 1 : null;
     }
 
     public static <T> Pageable<T> of(Collection<T> content, PageRequest request, int totalElements) {
